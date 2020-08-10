a3d6c250ed54dd643ab15166ec93beb6b410a9ee
anjali2001106 <68964827+anjali2001106@users.noreply.github.com>
2020-08-10 09:00:18 -0700

    Add files via upload

---
+++ Assignment/Assignment2/prog1_a.cpp
@@ -0,0 +1,23 @@
+//First Pattern Square box
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,r,c;
+    cout<<"Enter number of rows and columns :";
+    cin>>r>>c;
+    for(i=1;i<=r;i++)
+    {
+        for(j=1;j<=c;j++)
+        {
+            if(i>1&&i<c&&j>1&&j<c)
+            cout<<" ";
+            else if(i==1 || i==9)
+            cout<<"*";
+            else
+            cout<<"*";
+            
+        }
+        cout<<"\n";
+    }
+}
