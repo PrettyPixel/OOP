edf14167135e6e6c423cf8ee85e68be33822572d
anjali2001106 <68964827+anjali2001106@users.noreply.github.com>
2020-08-10 08:55:30 -0700

    Add files via upload

---
+++ Assignment/Assignment1/prog1_greatest.cpp
@@ -0,0 +1,15 @@
+//program to find the greatest of three numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"Enter three numbers :"<<endl;
+    cin>>a>>b>>c;
+    if(a>b&&a>c)
+    cout<<a<<" is the greatest number";
+    else if(b>a&&b>c)
+    cout<<b<<" is the greatest number";
+    else
+    cout<<c<<" is the greatest number";
+}
